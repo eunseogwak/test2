8f707a27fd9e14e992a3cacb65186ad911bf462f
eunseogwak <rhkr1710@naver.com>
2022-04-28 14:41:07 +0900

    commit

---
+++ RectArrayTest.java
@@ -0,0 +1,12 @@
+public class RectArrayTest {
+    public static void main(String[] args) {
+        Rect[] list;                 //참조변수 선언
+        list = new Rect[5];            //배열 객체 생성
+
+        for(int i=0;i < list.length; i++) //배열 요소 생성
+            list[i] = new Rect(i, i);
+
+        for(int i=0;i <list.length; i++)  //배열 요서 처리
+            System.out.println(i+"번째 사각형의 면적"+list[i].getArea());
+    }
+}
